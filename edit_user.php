85fde9acb7259f3e0ca2fbf87e2b63d2807f6bb2
Owen <s3492611@student.rmit.edu.au>
2020-05-25 20:40:34 +1000

    dfdsfdsfdsf

--- edit_user.php
+++ edit_user.php
@@ -2,52 +2,17 @@
 <body>
     <h1>Edit a User</h1>
   
-   <form action="tours.php" method="POST">
+    <?php
+    $servername = "sepmdb";
+    $username = "root";
+    $password = "null";
+    $conn = new mysqli($servername, $username, $password);
 
-Select User:  <select name="user" class="bl-select">
-
- <?php
-   
-   
-   $servername = '35.244.97.104';
-$dbname = 'sepmdb';
-$dbUser = 'root';
-$dbPass = null;
-$db = new mysqli($servername, $dbUser, $dbPass, $dbname);
-
-if ($db->connect_error) {
-  die("Connection failed: " . $db->connect_error);
-}     
-
-$sql = "SELECT username FROM users";
-$result = $db->query($sql);  
-  
-$array = array();  
-#if ($db->query($sql) === TRUE) {
-#} else {
-#  echo "Error: " . $sql . "<br>" . $db->error;
-#}
- if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-    echo " - Name: " . $row["user"]. "<br>";
-    $array =$row["user"];
+    if ($con->connection_error){
+        die("Connection failed: " . $conn->connection_error);
+    }
+    echo "Connected successfully";
     ?>
-        <option value="<?php echo $row["user"];?>"><?php echo $row["user"];?></option>
-<?php
-  }
-} else {
-  echo "0 results";
-} 
-   ?>
-   
 
-
-</select>
-     <br><br/>
-     <button type="submit" >Submit to register</button>
-</form>
- 
-    
 </body>
 </html> 
