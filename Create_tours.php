383f5d959d92f2d4fac6dc34dcb64e08b2c4c98b
Mike <mickeybitey@hotmail.com>
2020-05-25 15:06:40 +1000

    Glub glub

--- Create_tours.php
+++ Create_tours.php
@@ -2,6 +2,13 @@
 <html>
 <body>
     <h1>Create a new Tour</h1>
+  
+   <form action="tours.php" method="POST">
+
+Tour name: <input type="text" name="tourname" id="tourname" required>
+<br/><br/>
+First location:  <select name="location" class="bl-select">
+
  <?php
    
    
@@ -18,6 +25,7 @@ if ($db->connect_error) {
 $sql = "SELECT exhibitname FROM locations";
 $result = $db->query($sql);  
   
+$array = array();  
 #if ($db->query($sql) === TRUE) {
 #} else {
 #  echo "Error: " . $sql . "<br>" . $db->error;
@@ -26,28 +34,18 @@ $result = $db->query($sql);
   // output data of each row
   while($row = $result->fetch_assoc()) {
     echo " - Name: " . $row["exhibitname"]. "<br>";
+    $array =$row["exhibitname"];
+    ?>
+        <option value="<?php echo $row["exhibitname"];?>"><?php echo $row["exhibitname"];?></option>
+<?php
   }
 } else {
   echo "0 results";
 } 
+   ?>
    
-   
-   
-   
-   # $fileHandle = fopen("locations.csv", "r");
-   # while (($row = fgetcsv($fileHandle, 0, ",")) !== FALSE) {
-    #    $location = $row[0];
-?>
- <form action="tours.php" method="POST">
 
-Tour name: <input type="text" name="tourname" id="tourname" required>
-<br/><br/>
-First location:  <select name="location" class="bl-select">
 
-    <option value="<?php echo $row["exhibitname"];?>"><?php echo $row["exhibitname"];?></option>
-<?php
-#	}
-?>
 </select>
      <br><br/>
      <button type="submit" >Submit to register</button>
