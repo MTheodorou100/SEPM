5f39ca302f439cd8047713b29dd267072cc5c24f
Mike <mickeybitey@hotmail.com>
2020-05-25 14:58:32 +1000

    glub glub

--- Create_tours.php
+++ Create_tours.php
@@ -2,20 +2,51 @@
 <html>
 <body>
     <h1>Create a new Tour</h1>
- 
+ <?php
+   
+   
+   $servername = '35.244.69.181';
+$dbname = 'SEPMdb';
+$dbUser = 'root';
+$dbPass = null;
+$db = new mysqli($servername, $dbUser, $dbPass, $dbname);
+
+if ($db->connect_error) {
+  die("Connection failed: " . $db->connect_error);
+}     
+
+$sql = "SELECT exhibitname FROM locations";
+$result = $db->query($sql);  
+  
+#if ($db->query($sql) === TRUE) {
+#} else {
+#  echo "Error: " . $sql . "<br>" . $db->error;
+#}
+ if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo " - Name: " . $row["exhibitname"]. "<br>";
+  }
+} else {
+  echo "0 results";
+} 
+   
+   
+   
+   
+   # $fileHandle = fopen("locations.csv", "r");
+   # while (($row = fgetcsv($fileHandle, 0, ",")) !== FALSE) {
+    #    $location = $row[0];
+?>
  <form action="tours.php" method="POST">
 
 Tour name: <input type="text" name="tourname" id="tourname" required>
 <br/><br/>
 First location:  <select name="location" class="bl-select">
+
+    <option value="<?php echo $row["exhibitname"];?>"><?php echo $row["exhibitname"];?></option>
 <?php
-    $fileHandle = fopen("locations.csv", "r");
-    while (($row = fgetcsv($fileHandle, 0, ",")) !== FALSE) {
-        $location = $row[0];
-?>
-    <option value="<?php echo $location;?>"><?php echo $location;?></option>
-<?php
-	}
+#	}
 ?>
 </select>
      <br><br/>
