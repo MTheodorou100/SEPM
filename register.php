fa144afbc3ea2b13c8be1ea8d786bda8a862dd90
Mike <mickeybitey@hotmail.com>
2020-05-25 13:38:39 +1000

    DBconnect

--- register.php
+++ register.php
@@ -6,17 +6,33 @@
 	<link rel="stylesheet" href="../client/css.css">
 <body>  
 	<?php
+$servername = '35.244.69.181';
+$dbname = 'SEPMdb';
+$dbUser = 'root';
+$dbPass = null;
+$db = new mysqli($servername, $dbUser, $dbPass, $dbname);
 
+if ($db->connect_error) {
+  die("Connection failed: " . $db->connect_error);
+}     
 
-    
-    
-$file = fopen('users.csv','a+'); //"w"=overwrite
-fwrite($file, $_POST['username']);
-fwrite($file, ",");
-fwrite($file, $_POST["password"]); 
-fwrite($file, ",");
-fwrite($file, $_POST["account"].PHP_EOL);
-fclose($file);
+$sql = "INSERT INTO users (username, password, acctype)
+VALUES ('John', 'Doe', 'admin')";
+  
+if ($db->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $db->error;
+}
+  
+
+#$file = fopen('users.csv','a+'); //"w"=overwrite
+#fwrite($file, $_POST['username']);
+#fwrite($file, ",");
+#fwrite($file, $_POST["password"]); 
+#fwrite($file, ",");
+#fwrite($file, $_POST["account"].PHP_EOL);
+#fclose($file);
 ?>
 	<h1>The user has been registered!</h1>
 	<p> <a href="http://titan.csit.rmit.edu.au/~s3722979/SEPM/">Click here</a> to return to main directory</p>
