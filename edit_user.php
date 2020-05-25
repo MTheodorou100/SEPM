eff64736d339f80ae9c9d082b5e7394b33b59a7c
Owen <s3492611@student.rmit.edu.au>
2020-05-25 21:03:56 +1000

    zcdvgbfdxzhg

--- edit_user.php
+++ edit_user.php
@@ -1,17 +1,38 @@
 <html>
 <body>
     <h1>Edit a User</h1>
-  
-    <?php
-    $servername = "sepmdb";
-    $username = "root";
-    $password = "null";
-    $conn = new mysqli($servername, $username, $password);
+    
+   Select User: <select name="users" class="dropbtn">Users</select>
 
-    if ($con->connection_error){
+    <?php
+    $servername = '35.244.97.104';
+    $dbname = 'SEPMdb';
+    $username = 'root';
+    $password = null;
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if (!$conn){
         die("Connection failed: " . $conn->connection_error);
     }
-    echo "Connected successfully";
+
+    $sql = "SELECT username FROM users";
+    $result = $db->query($sql);
+
+    $array = array(); 
+
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            echo " - Name: " . $row["username"]. "<br>";
+            $array =$row["username"];
+            ?>
+                <option value="<?php echo $row["username"];?>"><?php echo $row["username"];?></option>
+        <?php
+          }
+        } else {
+          echo "0 results";
+        } 
+           ?>
+    }
     ?>
 
 </body>
