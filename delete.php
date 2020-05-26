916c1209194de20171b9cb9e22270cf188aa09cf
Owen <s3492611@student.rmit.edu.au>
2020-05-26 12:38:49 +1000

    connection

--- delete.php
+++ delete.php
@@ -1,7 +1,16 @@
 <?php
+$servername = '35.244.97.104';
+$dbname = 'SEPMdb';
+$username = 'root';
+$password = null;
+$conn = new mysqli($servername, $username, $password, $dbname);
 
+if (!$conn){
+    die("Connection failed: " . $conn->connection_error);
+    echo "not connected";
+}
 //Define the query
-$query = "DELETE FROM users WHERE username={$_POST['username']} LIMIT 1";
+$query = ($conn, "DELETE FROM users WHERE username={$_POST['username']} LIMIT 1");
 
 //sends the query to delete the entry
 mysqli_query ($query);
