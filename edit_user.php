e8785b12ea1ac56537b687593c230daf12c94d1d
Owen <s3492611@student.rmit.edu.au>
2020-05-26 11:16:12 +1000

    shows user in dropdown

--- edit_user.php
+++ edit_user.php
@@ -1,55 +1,35 @@
 <html>
 <body>
-    <h1>Create a new Tour</h1>
-  
-   <form action="tours.php" method="POST">
-
-Tour name: <input type="text" name="tourname" id="tourname" required>
-<br/><br/>
-First location:  <select name="location" class="bl-select">
-
- <?php
-   
-   
-   $servername = '35.244.69.181';
-$dbname = 'SEPMdb';
-$dbUser = 'root';
-$dbPass = null;
-$db = new mysqli($servername, $dbUser, $dbPass, $dbname);
-
-if ($db->connect_error) {
-  die("Connection failed: " . $db->connect_error);
-}     
-
-$sql = "SELECT exhibitname FROM locations";
-$result = $db->query($sql);  
-  
-$array = array();  
-#if ($db->query($sql) === TRUE) {
-#} else {
-#  echo "Error: " . $sql . "<br>" . $db->error;
-#}
- if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-    echo " - Name: " . $row["exhibitname"]. "<br>";
-    $array =$row["exhibitname"];
-    ?>
-        <option value="<?php echo $row["exhibitname"];?>"><?php echo $row["exhibitname"];?></option>
-<?php
-  }
-} else {
-  echo "0 results";
-} 
-   ?>
-   
-
-
-</select>
-     <br><br/>
-     <button type="submit" >Submit to register</button>
-</form>
- 
+    <h1>Edit a User</h1>
     
+   Select User: <select name="users" class="dropbtn">
+
+    <?php
+    $servername = '35.244.97.104';
+    $dbname = 'SEPMdb';
+    $username = 'root';
+    $password = null;
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if (!$conn){
+        die("Connection failed: " . $conn->connection_error);
+        echo "not connected";
+    }
+
+    $sql = mysqli_query($conn, "SELECT username FROM users");
+    
+
+    $array = array(); 
+
+    if ($sql->num_rows > 0) {
+        while($row = $sql->fetch_assoc()) {
+            echo " <option value=\user1\">" . $row['username'] . "</option>";
+            $array =$row["username"];
+          }
+        } else {
+          echo "0 results";
+    }
+    ?>
+    </select>
 </body>
 </html> 
