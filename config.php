c9352c80713fe53555148bd6e60fcea5489fddd3
Owen <s3492611@student.rmit.edu.au>
2020-05-29 14:19:09 +1000

    changed

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+   $servername = '35.244.97.104';
+   $dbname = 'SEPMdb';
+   $username = 'root';
+   $password = null;
+   $conn = new mysqli($servername, $username, $password, $dbname);
+   ?>
--- login.php
+++ login.php
@@ -1,10 +1,40 @@
+ <!DOCTYPE html>
+<html>
 <?php
-
-echo "Logged in as ?????";
-
-
-
-
-
-
+   include('config.php');
+   session_start();
+   
+   $user_check = $_SESSION['login_user'];
+   
+   $ses_sql = mysqli_query($conn,"select username from user where username = '$user_check' ");
+   
+   $row = mysqli_fetch_array($ses_sql, MYSQL_ASSOC);
+   
+   $login_session = $row['username'];
+   
+   if(!isset($_SESSION['login_user'])){
+      header("location:login.php");
+      die();
+   }
 ?>
+<body>
+
+<h1>Login</h1>
+<p>Welcome to the login page, enter your username and password below</p><br>
+    
+<form action="login.php" method="post">
+    
+ <label for="uname">Username:</label><br>   
+ <input type="text" id="uname" name="uname"><br><br>   
+    
+ <label for="pword">Password:</label><br>   
+ <input type="password" id="pword" name="pword"><br>
+ <br>
+    
+ <input type="submit" value="Login">
+    
+    
+</form>    
+
+</body>
+</html>
